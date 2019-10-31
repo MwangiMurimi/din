a8fef55aef95312404b0b55e15bd5b331d623479
MwangiMurimi <mwangimurimi15@gmail.com>
2019-10-31 16:54:17 +0300

    make last changes

--- js/scripts.js
+++ js/scripts.js
@@ -2,49 +2,50 @@ function x() {
   var date = document.getElementById("date").value;
   var d = new Date(date);
   var x = d.getDay();
-  var gender = document.getElementById("gender").value.toLowerCase();
+  const male = document.getElementById("male").checked;
+  const female = document.getElementById("female").checked;
   var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
   var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-  if (x === 0 && gender === "male") {
+  if (x === 0 && male) {
     document.getElementById("name").value = ("Your Akan name is " + maleNames[0] + ".")
   }
-  else if (x === 1 && gender === "male") {
+  else if (x === 1 && male) {
     document.getElementById("name").value = ("Your Akan name is " + maleNames[1] + ".")
   }
-  else if (x === 2 && gender === "male") {
+  else if (x === 2 && male) {
     document.getElementById("name").value = ("Your Akan name is " + maleNames[2] + ".")
   }
-  else if (x === 3 && gender === "male") {
+  else if (x === 3 && male) {
     document.getElementById("name").value = ("Your Akan name is " + maleNames[3] + ".")
   }
-  else if (x === 4 && gender === "male") {
+  else if (x === 4 && male) {
     document.getElementById("name").value = ("Your Akan name is " + maleNames[4] + ".")
   }
-  else if (x === 5 && gender === "male") {
+  else if (x === 5 && male) {
     document.getElementById("name").value = ("Your Akan name is " + maleNames[5] + ".")
   }
-  else if (x === 6 && gender === "male") {
+  else if (x === 6 && male) {
     document.getElementById("name").value = ("Your Akan name is " + maleNames[6] + ".")
   }
-  else if (x === 0 && gender === "female") {
+  else if (x === 0 && female) {
     document.getElementById("name").value = ("Your Akan name is " + femaleNames[0] + ".")
   }
-  else if (x === 1 && gender === "female") {
+  else if (x === 1 && female) {
     document.getElementById("name").value = ("Your Akan name is " + femaleNames[1] + ".")
   }
-  else if (x === 2 && gender === "female") {
+  else if (x === 2 && female) {
     document.getElementById("name").value = ("Your Akan name is " + femaleNames[2] + ".")
   }
-  else if (x === 3 && gender === "female") {
+  else if (x === 3 && female) {
     document.getElementById("name").value = ("Your Akan name is " + femaleNames[3] + ".")
   }
-  else if (x === 4 && gender === "female") {
+  else if (x === 4 && female) {
     document.getElementById("name").value = ("Your Akan name is " + femaleNames[4] + ".")
   }
-  else if (x === 5 && gender === "female") {
+  else if (x === 5 && female) {
     document.getElementById("name").value = ("Your Akan name is " + femaleNames[5] + ".")
   }
-  else if (x === 6 && gender === "female") {
+  else if (x === 6 && female) {
     document.getElementById("name").value = ("Your Akan name is " + femaleNames[6] + ".")
   }
   else {
